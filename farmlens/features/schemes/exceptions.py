8a68026b3cad595644ed3d933732cf96d9fe53a2
Amit-Yadav316 <yadavineet9198@gmail.com>
2026-06-08 18:20:19 +0530

    Add farmlens/features/schemes/exceptions.py
    Add file farmlens/features/schemes/exceptions.py (added by assistant).

---
+++ farmlens/features/schemes/exceptions.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from farmlens.core.exceptions import FarmLensException
+
+
+class SchemeException(FarmLensException):
+    """Raised when government scheme lookup fails."""
