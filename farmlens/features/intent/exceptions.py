53969c8223e0df94ac8095e0aed50491a6223f61
Amit-Yadav316 <yadavineet9198@gmail.com>
2026-06-08 18:20:18 +0530

    Add farmlens/features/intent/exceptions.py
    Add file farmlens/features/intent/exceptions.py (added by assistant).

---
+++ farmlens/features/intent/exceptions.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from farmlens.core.exceptions import FarmLensException
+
+
+class IntentException(FarmLensException):
+    """Raised when intent routing fails."""
